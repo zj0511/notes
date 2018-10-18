324f6efa014f7cfde2e80ee2e224cda12ef38d1b
杰丿瑞 <zj871619917@163.com>
2018-10-18 15:32:08 +0800

    备份

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import bookList from '@/views/books/booklist'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '图书列表',
+      component: bookList
     }
   ]
 })
---
+++ web/src/views/books/booklist.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>图书列表</div>
+</template>
+
+<script>
+  export default {
+    name: 'bookList'
+  }
+</script>
+
+<style scoped>
+
+</style>
